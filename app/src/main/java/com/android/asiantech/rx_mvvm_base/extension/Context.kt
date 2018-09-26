2b0556131e43f68d133aa4fa2b15108eb7efc2f4
vinhhuynhl.b <vinh.huynh@asiantech.vn>
2018-09-26 15:56:03 +0700

    remove code unneeded

--- app/src/main/java/com/android/asiantech/rx_mvvm_base/extension/Context.kt
+++ app/src/main/java/com/android/asiantech/rx_mvvm_base/extension/Context.kt
@@ -2,24 +2,12 @@ package com.android.asiantech.rx_mvvm_base.extension
 
 import android.content.Context
 import android.support.v7.app.AlertDialog
-import android.util.DisplayMetrics
-import android.view.WindowManager
 import com.android.asiantech.rx_mvvm_base.R
-
 
 /**
  *
  * @author at-vinhhuynh
  */
-
-
-internal fun Context.getWidthScreen(): Int {
-    val wm = getSystemService(Context.WINDOW_SERVICE) as? WindowManager
-    val dimension = DisplayMetrics()
-    wm?.defaultDisplay?.getMetrics(dimension)
-    return dimension.widthPixels
-}
-
 internal fun Context.showAlert(title: Int, message: String, onOkClick: () -> Unit = {}) {
     val alertBuilder = AlertDialog.Builder(this)
     alertBuilder.setTitle(title)
