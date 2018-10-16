72143f925ec1ce5bc935ac8d14b47e06f451935c
at-huongnguyen <huong.nguyen@asiantech.vn>
2018-10-16 10:55:05 +0700

    Refactor code

--- app/src/main/java/com/android/asiantech/rx_mvvm_base/data/source/remote/network/ApiClient.kt
+++ app/src/main/java/com/android/asiantech/rx_mvvm_base/data/source/remote/network/ApiClient.kt
@@ -7,7 +7,6 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Protocol
 import okhttp3.ResponseBody
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -39,9 +38,6 @@ open class ApiClient private constructor(url: String? = null) {
 
     private fun createService(): ApiService {
         val httpClientBuilder = OkHttpClient.Builder()
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        httpClientBuilder.addInterceptor(interceptor)
         httpClientBuilder.interceptors().add(Interceptor { chain ->
             val original = chain.request()
             // Request customization: add request headers
