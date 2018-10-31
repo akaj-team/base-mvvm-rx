2118f7b5c9908c13a9827d4ff20cb61fb09b0712
at-trungnguyen <11608143+at-trungnguyen@users.noreply.github.com>
2018-10-31 17:34:01 +0700

    fix commic detail

--- app/src/main/java/com/android/asiantech/rx_mvvm_base/extension/Disposable.kt
+++
@@ -1,7 +0,0 @@
-package com.android.asiantech.rx_mvvm_base.extension
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-fun Disposable.addTo(compositeDisposable: CompositeDisposable): Disposable
-        = apply { compositeDisposable.add(this) }
